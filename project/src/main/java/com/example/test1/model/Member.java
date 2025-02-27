05b6e168709c4d174d219164852c46da55fd1390
codingjs12 <202245068@itc.ac.kr>
2025-02-27 09:51:33 +0900

    20250227 spring

---
+++ project/src/main/java/com/example/test1/model/Member.java
@@ -0,0 +1,31 @@
+package com.example.test1.model;
+
+import lombok.Data;
+
+@Data
+public class Member {
+	
+	private String userId;
+	private String userName;
+	private String address;
+	
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+}
